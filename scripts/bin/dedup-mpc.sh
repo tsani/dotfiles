fa750038747770c888339aff7a16b518034284bc
Jacob Errington <git@mail.jerrington.me>
2018-04-05 14:36:35 -0400

    add script for deduplicating MPD playlist

--- scripts/bin/dedup-mpc.sh
+++ scripts/bin/dedup-mpc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mpc playlist | nl | sort -k 2 | uniq -f 1 -d | sort -nr | cut -f 1 | xargs mpc del
+mpc playlist | nl | sort -k 2 | uniq -f 1 -d | sort -nr | cut -f 1 | tee >(xargs mpc del)
