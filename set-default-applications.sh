308c6448179600aedf131ec89d21b57a2cf3a2da
Jacob Errington <git@mail.jerrington.me>
2020-01-10 17:32:18 -0500

    use atril by default for pdf

--- set-default-applications.sh
+++ set-default-applications.sh
@@ -3,5 +3,6 @@
 set -e
 
 # use BROWSER hack to avoid error about BROWSER being set
-BROWSER= xdg-settings set default-web-browser vimb.desktop
+# BROWSER= xdg-settings set default-web-browser vimb.desktop
 xdg-mime default feh.desktop 'image/*'
+xdg-mime default atril.desktop 'application/pdf'
